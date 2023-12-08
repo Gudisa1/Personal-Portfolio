3ef4cd03fbd97e4c756c223052e32a009789f5b2
Gudisa Gebi <gudisagebi1@gmail.com>
2023-12-08 08:27:46 -0800

    Personal Portfolio

---
+++ Pictures/portfolio/src/components/experience/index.js
@@ -0,0 +1,75 @@
+const experiences = [
+  {
+    title: "Software Development Intern",
+    company_name: "Ethio Click",
+    icon: "starbucks",
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    points: [
+      "Completed an internship at Ethio Click, focusing on Java-based technologies, including Servlet, JSP, and Spring Boot.",
+      "Contributed to the development and maintenance of web applications using Java and related frameworks.",
+      "Implemented responsive design principles and ensured cross-browser compatibility for a seamless user experience.",
+    ],
+  },
+
+  {
+    title: "Freelance React.js Developer",
+    company_name: "Freelancing",
+    icon: "shopify",
+    iconBg: "#383E56",
+    date: "August 2021 - ----",
+    points: [
+      "Engaged in freelance projects as a React.js Developer with a specialization in Tailwind CSS and Material UI.",
+      "Led the development and maintenance of diverse web applications using React.js and other cutting-edge technologies.",
+      "Collaborated effectively with cross-functional teams, including designers, product managers, and fellow developers, to create bespoke and high-quality products.",
+      "Implemented unique and responsive design solutions, ensuring optimal cross-browser compatibility.",
+      "Contributed to collaborative code reviews, offering constructive feedback to enhance the overall development process.",
+    ],
+  },
+  {
+    title: "Full Stack MERN Developer",
+    company_name: "Grojobs",
+    icon: "meta",
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Currently serving as a Full Stack MERN Developer at Grojobs, contributing to the development and maintenance of robust web applications.",
+      "Utilizing React.js and other advanced MERN stack technologies to create scalable and high-performance web solutions.",
+      "Collaborating seamlessly with cross-functional teams, including designers, product managers, and fellow developers, to achieve the highest standards of product quality.",
+      "Implementing and refining responsive design principles, ensuring an exceptional user experience across various browsers.",
+      "Actively participating in collaborative code reviews, providing constructive feedback to foster a continuous improvement-driven development environment.",
+    ],
+  },
+];
+
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Gudisa proved me wrong.",
+//     name: "Amanuel Alemayehu",
+//     designation: "CFO",
+//     company: "Amano Production",
+//     image:
+//       "https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/273964857_1630257104039338_7208099309175242809_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=XNgxLzyjtG4AX-9ZWZ-&_nc_ht=scontent-ams4-1.xx&oh=00_AfDuwtsm120py_d8ppcX-tz05FseHfcdZCA8wKYMwG5ECQ&oe=641B0ABA",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Gudi does.",
+//     name: "Firew Bahiru",
+//     designation: "COO",
+//     company: "Halel",
+//     image:
+//       "https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/332124015_2840672586066373_1754360653441089436_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=7A6uYHRH3_EAX9ls3Qg&_nc_ht=scontent-ams4-1.xx&oh=00_AfAPo54oG1eOL0LMJf5m6oosVPpevBsG70AJc5qgMcle8g&oe=641A4682",
+//   },
+//   {
+//     testimonial:
+//       "After Gudisa optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Niftalem Mulu",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image:
+//       "https://scontent-ams2-1.xx.fbcdn.net/v/t39.30808-6/305515828_1746850085671317_1726008122688268147_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pvHX07aIJUgAX-BnLKg&_nc_ht=scontent-ams2-1.xx&oh=00_AfDeZo2kuZ8jxpktvybowHu6MLmAS47I21Ri4WJysqpc9A&oe=641A9DEB",
+//   },
+// ];
+
+export { experiences };
---
+++ Pictures/portfolio/src/components/hero/Hero.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./hero.scss";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+import { motion } from "framer-motion";
+const textVariants = {
+  initial: {
+    x: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 1, staggerChildren: 0.1 },
+    scrollButton: {
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: { repeat: Infinity, duration: 10, repeatType: "mirror" },
+  },
+};
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}> Gudisa Gebi</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web Developer &amp; Content Creator
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              Download Resume
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <KeyboardDoubleArrowDownIcon
+            variants={textVariants}
+            className="img"
+            animate="scrollButton"
+          />
+        </motion.div>
+        <motion.div
+          className="slidingTextContainer"
+          variants={sliderVariants}
+          initial="initial"
+          animate="animate"
+        >
+          Frontend Enginner and Content Creator
+        </motion.div>
+        <div className="imageContainer">
+          <img src="gudisa.jpg" alt="The Developer Picture" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
